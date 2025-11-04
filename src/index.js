3f685bb378f25d463e0a273386b162231a3f630f
huxrce <its.erica.hu@gmail.com>
2025-11-04 17:22:57 +0100

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ function generatePoem(event) {
     "You are an expert on short poems about any given topic. Please generate a 4 line rhyming poem in basic HTML format, separating each line with a <br />. Do not include a title to the poem. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning.";
   let prompt = `User instructions: generate a poem about ${promptInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating a poem about ${promptInput.value}...</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
